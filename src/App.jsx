742feb4ed34be34d389c605f32fa5037fdddc2ac
shivv2430 <2430.shivanipal@gmail.com>
2026-04-15 20:58:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import logo from './assets/IngreChef logo design.png'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div className='Navbar'>
+        <img src={logo} alt="logo" className='logo' />
+        <h2 >IngreChef</h2>
+
+      </div>
+    </div>
   )
 }
 
